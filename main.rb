1ecf45a17be42702c803398cea7d3510e9287d51
Sriram Krishnan <sriramank@live.in>
2020-01-30 11:48:32 +1100

    is development logic changed

--- main.rb
+++ main.rb
@@ -2,17 +2,18 @@ require 'sinatra'
 if development?
   require 'sinatra/reloader'
   require 'pry'
-  require_relative 'shared.rb'
-  require_relative 'models/users.rb'
-  require_relative 'models/product.rb'
-  require_relative 'controllers/product.rb'
-  require_relative 'controllers/user.rb'
   also_reload 'models/users'
   also_reload 'shared.rb'
   also_reload 'models/product.rb'
   also_reload 'controllers/product.rb'
   also_reload 'controllers/user.rb'
 end
+require_relative 'shared.rb'
+  require_relative 'models/users.rb'
+  require_relative 'models/product.rb'
+  require_relative 'controllers/product.rb'
+  require_relative 'controllers/user.rb'
+  
 enable :sessions
 
 get '/' do
